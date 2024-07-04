922ad93d0afad59b6bf8ca8721a758457eb00fc4
Manassés <70027468+manassespaterline@users.noreply.github.com>
2024-07-03 21:47:03 -0300

    Version 2.0
    I made an animation for the sidebar, and added a color picker that allows the user choose all the RGB colors!

--- config.py
+++ config.py
@@ -1,19 +1,28 @@
 import pygame
 pygame.init()
 
-canvasSize = canvasWidth, canvasHeight = 720, 480
+canvasSize = canvasWidth, canvasHeight = 800, 600
 canvas = pygame.display.set_mode(canvasSize)
+pygame.display.set_caption("Penting")
 
 # Variables
 bg_color = (48, 48, 48)
 drawing = False
 
 brushSize = 4
-brushColor = ((255, 255, 255))
+brushColor = (255, 255, 255)
 canvasColor = ((48, 48, 48))
 
 sidebar = False
 tools_height = 0
 tool_size = 30
+
+tool1_h = 30
+tool1_y = 70
+
+
+tool2_h = 30
+tool3_h = 30
+tool4_h = 30
 
 
--- main.py
+++ main.py
@@ -1,15 +1,87 @@
 import pygame, sys
 from pygame.locals import *
 from config import *
+import pygame_gui
+
+from pygame_gui.elements import UIButton
+from pygame_gui.windows import UIColourPickerDialog
 
 pygame.init()
 
-canvas.fill(bg_color)
+drawn_positions = []
+
+
+def colorPicker():
+    global brushColor
+    global drawing
+    ui_manager = pygame_gui.UIManager((800, 600))
+    #background = pygame.Surface((800, 600))
+    #background.fill("#3a3b3c")
+
+    colour_picker_button = UIButton(relative_rect=pygame.Rect(-700, -500, 150, 30),
+                                text='Pick Colour',
+                                manager=ui_manager,
+                                anchors={'left': 'right',
+                                        'right': 'right',
+                                        'top': 'bottom',
+                                        'bottom': 'bottom'})
+    colour_picker = None                                    
+    current_colour = pygame.Color(0, 0, 0)
+    #picked_colour_surface = pygame.Surface((400, 400))
+    #picked_colour_surface.fill(current_colour)
+
+    clock = pygame.time.Clock()
+
+    running_colorpicker = True
+    while running_colorpicker:
+        time_delta = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == colour_picker_button:
+                colour_picker = UIColourPickerDialog(pygame.Rect(160, 50, 420, 400),
+                                                    ui_manager,
+                                                    window_title="Change Colour...",
+                                                    initial_colour=current_colour)
+                colour_picker_button.disable()
+            if event.type == pygame_gui.UI_COLOUR_PICKER_COLOUR_PICKED:
+                current_colour = event.colour
+                #picked_colour_surface.fill(current_colour)
+                brushColor = current_colour
+                drawing = False
+                running_colorpicker = False
+            if event.type == pygame_gui.UI_WINDOW_CLOSE:
+                colour_picker_button.enable()
+                colour_picker = None
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_o:
+                    running_colorpicker = False
+            
+            ui_manager.process_events(event)
+            
+        ui_manager.update(time_delta)
+
+        #canvas.blit(background, (0, 0))
+        #canvas.blit(picked_colour_surface, (200, 100))
+
+        ui_manager.draw_ui(canvas)
+
+        pygame.display.update()
+
 
 running = True
 while running:
 
+    canvas.fill(bg_color)
     mouse_pos = mouse_x, mouse_y = pygame.mouse.get_pos()
+
+    tools = pygame.Rect(15, 60, 40, tools_height)
+    tool_1 = pygame.Rect(20, tool1_y, tool_size, tool1_h)
+    tool_2 = pygame.Rect(20, 110, tool_size, tool2_h)
+    tool_3 = pygame.Rect(20, 150, tool_size, tool3_h)
+    tool_4 = pygame.Rect(20, 190, tool_size, tool4_h)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,22 +90,26 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 drawing = True
-
                 if open_sidebar.collidepoint(mouse_pos):
                     if sidebar:
                         sidebar = False
-                        canvas.fill(bg_color)
                     else:
                         sidebar = True
+
+                 #Open color picker
+                if tool_1.collidepoint(mouse_pos):
+                    colorPicker()
+
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 drawing = False
 
     if drawing:
-        pygame.draw.circle(canvas, 'white', (mouse_x, mouse_y), 8)
-        print('drawing')
-    else:
-        print("not drawing")
+        drawn_positions.append(((mouse_x, mouse_y), brushColor))
+
+    # Desenhe todas as posições armazenadas
+    for pos, color in drawn_positions:
+        pygame.draw.circle(canvas, color, pos, 8)
 
     #Open sidebar
     open_sidebar = pygame.Rect(15, 15, 40, 40)
@@ -44,21 +120,24 @@ while running:
         if tools_height < 400:
             tools_height += 10
 
-        tools = pygame.Rect(15, 60, 40, tools_height)
         pygame.draw.rect(canvas, 'black', tools)
-
-        tool_1 = pygame.Rect(20, 70, tool_size, tool_size)
         pygame.draw.rect(canvas, (100, 100, 100), tool_1)
-
-        tool_2 = pygame.Rect(20, 110, tool_size, tool_size)
         pygame.draw.rect(canvas, (100, 100, 100), tool_2)
-
-        tool_3 = pygame.Rect(20, 150, tool_size, tool_size)
         pygame.draw.rect(canvas, (100, 100, 100), tool_3)
-
-        tool_4 = pygame.Rect(20, 190, tool_size, tool_size)
         pygame.draw.rect(canvas, (100, 100, 100), tool_4)
-    else:
-        tools_height = 0
+
+    elif not sidebar:
+        if tools_height > 0:
+            tools_height -= 10
+
+            pygame.draw.rect(canvas, 'black', tools)
+            pygame.draw.rect(canvas, (100, 100, 100), tool_1)
+            pygame.draw.rect(canvas, (100, 100, 100), tool_2)
+            pygame.draw.rect(canvas, (100, 100, 100), tool_3)
+            pygame.draw.rect(canvas, (100, 100, 100), tool_4)
+
+        
+
+    print(tools_height)
 
     pygame.display.update()
